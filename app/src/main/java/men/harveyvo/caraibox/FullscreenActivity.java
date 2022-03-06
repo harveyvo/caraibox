8439a8cadf2fefa1ec1ec63be1ad51b8bd77aa98
Loi Vo <ldcc.loivo@lotte.vn>
2022-03-06 23:43:34 +0700

    add request audio focus and open google map by default.

--- app/src/main/java/men/harveyvo/caraibox/FullscreenActivity.java
+++ app/src/main/java/men/harveyvo/caraibox/FullscreenActivity.java
@@ -5,10 +5,13 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowInsets;
 
@@ -20,23 +23,12 @@ import men.harveyvo.caraibox.databinding.ActivityFullscreenBinding;
  */
 public class FullscreenActivity extends AppCompatActivity {
     /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
+
     private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
@@ -59,7 +51,7 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         }
     };
-    private View mControlsView;
+    private int focusGainResult;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
@@ -68,83 +60,75 @@ public class FullscreenActivity extends AppCompatActivity {
             if (actionBar != null) {
                 actionBar.show();
             }
-            mControlsView.setVisibility(View.VISIBLE);
         }
     };
-    private boolean mVisible;
     private final Runnable mHideRunnable = new Runnable() {
         @Override
         public void run() {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+
+    private final Runnable focusAudioRunnable = new Runnable() {
         @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            switch (motionEvent.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    if (AUTO_HIDE) {
-                        delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                    }
-                    break;
-                case MotionEvent.ACTION_UP:
-                    view.performClick();
-                    break;
-                default:
-                    break;
+        public void run() {
+            try {
+                AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                focusGainResult = audioManager.requestAudioFocus(null, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+            } catch (Exception exception) {
+                Log.e("AUDIO FOCUS", exception.getMessage());
             }
-            return false;
         }
     };
+
+    private final Runnable abandonAudioRunnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                focusGainResult = audioManager.abandonAudioFocus(null);
+            } catch (Exception exception) {
+                Log.e("AUDIO FOCUS", exception.getMessage());
+            }
+        }
+    };
+
+    private final Runnable exitAppRunnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                finishAndRemoveTask();
+            } catch (Exception exception) {
+
+            }
+        }
+    };
+
     private ActivityFullscreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityFullscreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mVisible = true;
-        mControlsView = binding.fullscreenContentControls;
         mContentView = binding.fullscreenContent;
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        binding.dummyButton.setOnTouchListener(mDelayHideTouchListener);
+        delayedHide(100);
+        doAction();
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
+    private void doAction() {
+        mHideHandler.postDelayed(focusAudioRunnable, 0);
+        mHideHandler.postDelayed(abandonAudioRunnable, 1000);
+        openDefaultApp();
+        mHideHandler.postDelayed(exitAppRunnable, 3000);
     }
+
 
     private void hide() {
         // Hide UI first
@@ -152,28 +136,28 @@ public class FullscreenActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
         // Schedule a runnable to remove the status and navigation bar after a delay
         mHideHandler.removeCallbacks(mShowPart2Runnable);
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    private void show() {
-        // Show the system bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            mContentView.getWindowInsetsController().show(
-                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-        } else {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        }
-        mVisible = true;
+    private void openDefaultApp() {
+        Context ctx = this; // or you can replace **'this'** with your **ActivityName.this**
+        String firstApp = "com.vietmap.s1OBU";
+        String secondApp = "com.google.android.apps.maps";
+        try {
+            Intent i = ctx.getPackageManager().getLaunchIntentForPackage(firstApp);
+            ctx.startActivity(i);
+        } catch (Exception e) {
+            Log.e("Start App", e.getMessage());
+            try {
+                Intent i = ctx.getPackageManager().getLaunchIntentForPackage(secondApp);
+                ctx.startActivity(i);
+            } catch (Exception e2) {
+                Log.e("Start App", e.getMessage());
+            }
 
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+        }
     }
 
     /**
